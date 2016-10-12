f977ae4162a334b622772d7aff010c288db45a6b
Jonathan Guthrie <jono@perfect.org>
2016-10-12 16:37:47 -0400

    abstract so it's nested

--- Sources/parseRows.swift
+++ Sources/parseRows.swift
@@ -24,23 +24,41 @@ extension SQLiteStORM {
 	public func parseRows(_ result: [SQLiteStmt]) -> [StORMRow] {
 		var resultRows = [StORMRow]()
 		for row in result {
-			let this = StORMRow()
-			for i in 0..<row.columnCount() {
-				switch row.columnType(position: i) {
-				case 1:
-					this.data[row.columnName(position: i)] = row.columnInt(position: i)
-				case 2:
-					this.data[row.columnName(position: i)] = row.columnDouble(position: i)
-				case 4:
-					this.data[row.columnName(position: i)] = row.columnBlob(position: i)
-				// ignoring null, 5
-				default: // 3, string
-					this.data[row.columnName(position: i)] = String(row.columnText(position: i))
-				}
-
-			}
-			resultRows.append(this)
+//			let this = StORMRow()
+//			for i in 0..<row.columnCount() {
+//				switch row.columnType(position: i) {
+//				case 1:
+//					this.data[row.columnName(position: i)] = row.columnInt(position: i)
+//				case 2:
+//					this.data[row.columnName(position: i)] = row.columnDouble(position: i)
+//				case 4:
+//					this.data[row.columnName(position: i)] = row.columnBlob(position: i)
+//				// ignoring null, 5
+//				default: // 3, string
+//					this.data[row.columnName(position: i)] = String(row.columnText(position: i))
+//				}
+//
+//			}
+			resultRows.append(parseRow(row))
 		}
 		return resultRows
+	}
+	public func parseRow(_ row: SQLiteStmt) -> StORMRow {
+		let this = StORMRow()
+		for i in 0..<row.columnCount() {
+			switch row.columnType(position: i) {
+			case 1:
+				this.data[row.columnName(position: i)] = row.columnInt(position: i)
+			case 2:
+				this.data[row.columnName(position: i)] = row.columnDouble(position: i)
+			case 4:
+				this.data[row.columnName(position: i)] = row.columnBlob(position: i)
+			// ignoring null, 5
+			default: // 3, string
+				this.data[row.columnName(position: i)] = String(row.columnText(position: i))
+			}
+
+		}
+		return this
 	}
 }
