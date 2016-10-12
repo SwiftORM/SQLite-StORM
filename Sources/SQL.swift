32519c530399f5a439b884ae362752a81bbb7517
Jonathan Guthrie <jono@perfect.org>
2016-10-12 13:42:15 -0400

    refactoring some of the sql commands

--- Sources/SQL.swift
+++ Sources/SQL.swift
@@ -11,22 +11,10 @@ import SQLite
 
 extension SQLiteStORM {
 
-	/// Execute Raw SQL (with parameter binding)
-	/// Returns PGResult (discardable)
 	@discardableResult
-	public func sql(_ statement: String, params: [String]) throws {
+	public func sqlExec(_ statement: String) throws {
 		do {
-			try exec(statement, params: params)
-		} catch {
-			self.error = StORMError.error(error as! String)
-			throw error
-		}
-	}
-
-	@discardableResult
-	public func sql(_ statement: String, params: [String]) throws -> Any {
-		do {
-			return try execReturnID(statement, params: params)
+			try execStatement(statement)
 		} catch {
 			self.error = StORMError.error(error as! String)
 			throw error
@@ -44,7 +32,17 @@ extension SQLiteStORM {
 	}
 
 	@discardableResult
-	public func sql(_ statement: String, params: [String]) throws -> [StORMRow] {
+	public func sqlAny(_ statement: String, params: [String]) throws -> Any {
+		do {
+			return try execReturnID(statement, params: params)
+		} catch {
+			self.error = StORMError.error(error as! String)
+			throw error
+		}
+	}
+
+	@discardableResult
+	public func sqlRows(_ statement: String, params: [String]) throws -> [StORMRow] {
 		do {
 			return try execRows(statement, params: params)
 		} catch {
