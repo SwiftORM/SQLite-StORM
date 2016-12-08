1a4285afedef9e17f1a67464c154e733039ab079
Jonathan Guthrie <jono@perfect.org>
2016-12-08 11:34:28 -0500

    Update to use StORM 1.0

--- Package.swift
+++ Package.swift
@@ -1,20 +1,12 @@
-//
-//  Package.swift
-//  SQLite StORM
-//
-//  Created by Jonathan Guthrie on 2016-10-07.
-//	Copyright (C) 2016 Jonathan Guthrie.
-//
-
+// Generated automatically by Perfect Assistant Application
+// Date: 2016-12-08 16:23:26 +0000
 import PackageDescription
-
 let package = Package(
-	name: "SQLiteStORM",
-	targets: [],
-	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 0, minor: 0),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
-	],
-	exclude: []
+    name: "SQLiteStORM",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
+    ]
 )
