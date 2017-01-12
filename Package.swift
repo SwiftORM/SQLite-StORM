6506e3c595b500872084c53b8eb26ef4de15ad50
Jonathan Guthrie <jono@perfect.org>
2017-01-12 10:34:56 -0500

    update to latest logger

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-08 16:23:26 +0000
+// Date: 2017-01-12 15:33:46 +0000
 import PackageDescription
 let package = Package(
     name: "SQLiteStORM",
@@ -7,6 +7,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1, minor: 0),
     ]
 )
